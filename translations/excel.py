222023bca659850033b185e1b3f519f6039b3b1f
Tuyen Khong <Tuyen.Khong@dyson.com>
2023-08-09 11:49:23 +0100

    ADDED: Doc string

--- translations/excel.py
+++ translations/excel.py
@@ -1,9 +1,18 @@
 from openpyxl import load_workbook
 
 
-def read_excel(name):
+def read_excel(location):
+    '''
+    Ingest translations in excel format.
+
+        Param:
+            location (string): location of excel file xlsm
+        
+        Returns:
+            translations(dictionary): carry info extracted from excel
+    '''
     # Load work book
-    wb = load_workbook(name)
+    wb = load_workbook(location)
 
     # Load worksheet
     ws_name = wb.sheetnames[0]
